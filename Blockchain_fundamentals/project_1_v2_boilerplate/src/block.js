648fb657922feab9ac911e99e3ecbefe736e22cc
juan-ivan-NV <coldcram14@hotmail.com>
2022-08-05 23:33:56 -0500

    remix example

--- Blockchain_fundamentals/project_1_v2_boilerplate/src/block.js
+++ Blockchain_fundamentals/project_1_v2_boilerplate/src/block.js
@@ -39,9 +39,11 @@ class Block {
         let self = this;
         return new Promise((resolve, reject) => {
             // Save in auxiliary variable the current block hash
-            let currentHash = self.hash;                              
+            let currentHash = self.hash;
+            self.hash = null;                              
             // Recalculate the hash of the Block
-            let recalculatedHash = SHA256(JSON.stringify(self.hash)).toString();
+            let recalculatedHash = SHA256(JSON.stringify(self)).toString();
+            self.hash = recalculatedHash;
             // Comparing if the hashes changed
             // Returning the Block is not valid
             // Returning the Block is valid
