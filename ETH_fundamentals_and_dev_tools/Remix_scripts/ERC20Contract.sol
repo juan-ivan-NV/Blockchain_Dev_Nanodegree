3846410cac249222eed3813b2a8848d881ee5495
juan-ivan-NV <coldcram14@hotmail.com>
2022-08-24 13:37:23 -0500

    erc20 code

--- ETH_fundamentals_and_dev_tools/Remix_scripts/ERC20Contract.sol
+++ ETH_fundamentals_and_dev_tools/Remix_scripts/ERC20Contract.sol
@@ -2,22 +2,22 @@
 // ERC Token Standard #20 Interface
 // ------------------------------------------------------------
 
-pragma solidity ^0.4.25;
+pragma solidity ^0.4.24;
 
 contract ERC20Interface {
 
     string public constant name = "Udacity Token";
     string public constant symbol = "UDC";
-    uint8 public constant decimals = 18; // 18 is the most common number of decimal places 
+    uint8 public constant decimals = 18;  // 18 is the most common number of decimal places
 
-    event Transfer (address indexed from, address indexed to, uint tokens);
-    event Approval (address indexed tokenOwner, address indexed spender, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
-    function totalSupply() public constant return (uint);
-    function balanceOff(address takenOwner) public constant returns (uint balance);
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
     function transfer(address to, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
     function approve(address spender, uint tokens) public returns (bool success);
-    function allowance (address takeOwner, address spender) public constant returns (uint remaining);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
 
 }
