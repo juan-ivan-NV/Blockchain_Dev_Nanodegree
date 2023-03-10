b40a3d84d8b2dcf1473f31a76732c46b83c5a00f
juan-ivan-NV <coldcram14@hotmail.com>
2023-03-09 23:23:52 -0600

    write and read functions

--- solidity_tutorial/Counter.sol
+++ solidity_tutorial/Counter.sol
@@ -1,2 +1,18 @@
 pragma solidity ^0.8.0;
 
+contract Counter{
+    // State variable
+    uint count;
+
+    // "public" function can be accesed oustside the contract
+    // "view returns()" type of variable to return
+    // read functions are free
+    function getCount() public view returns(uint) {
+        return count;
+    }
+
+    // write functions require gas to be paid
+    function incrementAccount() public {
+        count += 1;
+    }
+}
