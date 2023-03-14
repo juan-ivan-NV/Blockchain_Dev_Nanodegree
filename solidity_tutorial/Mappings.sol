c0de0053fc53198772fe9e7122c6a787c4f9e214
juan-ivan-NV <coldcram14@hotmail.com>
2023-03-13 23:38:06 -0600

    mappings

---
+++ solidity_tutorial/Mappings.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract myContract {
+    // Mappings
+    mapping(uint => string) public names;
+    mapping(uint => Book) public books;
+    mapping(address => mapping(uint => Book)) public myBooks;
+
+    struct Book  {
+        string title;
+        string author;
+    }
+
+    constructor() {
+        names[1] = "yoo";
+        names[2] = "you";
+        names[3] = "yuu";
+    }
+
+    function addBook(
+        uint _id, 
+        string memory _title, 
+        string memory _author) public {
+        books[_id] = Book(_title, _author);
+    }
+
+    function addMyBook(
+        uint _id, 
+        string memory _title, 
+        string memory _author) public {
+        myBooks[msg.sender][_id] = Book(_title, _author);
+    }
+}
