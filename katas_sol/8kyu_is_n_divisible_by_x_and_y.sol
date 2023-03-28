ebb9179d42181d7444443d338b7a670a6d79c0d3
juan-ivan-NV <coldcram14@hotmail.com>
2023-03-27 19:43:04 -0600

    kata 8 kyu is n divisible best sol

--- katas_sol/8kyu_is_n_divisible_by_x_and_y.sol
+++ katas_sol/8kyu_is_n_divisible_by_x_and_y.sol
@@ -12,6 +12,19 @@ contract Kata {
   }
 }
 
+// best solution
+
+/*
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function isDivisible(int n, int x, int y) public pure returns (bool) {
+    return n % x == 0 && n % y == 0;
+  }
+}
+ */
+
+
 // Test cases
 
 /* 
