08782b487ffba9356e80c249e18b3d17422f9190
juan-ivan-NV <coldcram14@hotmail.com>
2023-04-09 01:03:51 -0600

    roman humeral encoder solution solidity

--- katas_sol/6kyu_roman_numerals_encoder.sol
+++ katas_sol/6kyu_roman_numerals_encoder.sol
@@ -2,18 +2,20 @@
 pragma solidity ^0.8.16;
 
 contract Kata {
-  
   function solution(uint256 n) public pure returns (string memory) {
     // Convert the positive integer to a Roman Numeral
     string memory rom_num;
     string[13] memory symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
     uint16[13] memory values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-        for(uint256 i; i < values.length; i+=1) {
-          if (n >= values[i]) {
-            rom_num = string(abi.encodePacked(rom_num, symbols[i]));
-            n = n - values[i];
-          }
-        }
+    uint i =0;
+    while (n > 0) {
+      if (n >= values[i]) {    
+        rom_num = string(abi.encodePacked(rom_num, symbols[i]));
+        n = n - values[i];
+      } else {
+        i++;
+      }
+    }
     return rom_num;
   }
 }
