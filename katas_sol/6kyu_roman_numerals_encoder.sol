40d5cccb2694cbe870993328e33ee86a87745f0b
juan-ivan-NV <coldcram14@hotmail.com>
2023-04-08 23:58:36 -0600

    failing some testings

--- katas_sol/6kyu_roman_numerals_encoder.sol
+++ katas_sol/6kyu_roman_numerals_encoder.sol
@@ -1,14 +1,19 @@
-// Convert the positive integer to a Roman Numeral
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  
+  function solution(uint256 n) public pure returns (string memory) {
+    // Convert the positive integer to a Roman Numeral
+    string memory rom_num;
     string[13] memory symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
     uint16[13] memory values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    string memory rom_num = "";
-    
-    
-    while (n > 0) {
-        for(uint i=0; i <= 13; i++) {
-          rom_num = string.concat(rom_num, symbols[i]);
-          n = n - values[i];
+        for(uint256 i; i < values.length; i+=1) {
+          if (n >= values[i]) {
+            rom_num = string(abi.encodePacked(rom_num, symbols[i]));
+            n = n - values[i];
+          }
         }
     return rom_num;
-    }
   }
+}
