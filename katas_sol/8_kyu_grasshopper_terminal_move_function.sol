9d126f18f8a588ef44c6ed5301948f480470c654
juan-ivan-NV <coldcram14@hotmail.com>
2023-05-01 23:02:12 -0600

    terminal game move function kata

---
+++ katas_sol/8_kyu_grasshopper_terminal_move_function.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function move(int p, int r) public pure returns (int) {
+    return p + r * 2;
+  }
+}
+
+
+/*
+// Testing code
+
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function move(int p, int r) public pure returns (int) {
+    return p + r * 2;
+  }
+}
+
+*/
